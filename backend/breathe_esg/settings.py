2d7a542a64822828c6d249b88e92e72d8a00adba
Sagar Prabhu <your-actual-email@gmail.com>
2026-05-27 19:11:16 +0530

    Fix SQLite path for Railway

--- backend/breathe_esg/settings.py
+++ backend/breathe_esg/settings.py
@@ -94,7 +94,7 @@ AUTH_USER_MODEL = 'core.User'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.environ.get('DB_PATH', BASE_DIR / 'db.sqlite3'),
     }
 }
 
