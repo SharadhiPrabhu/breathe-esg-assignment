fa5c65acd55e8e58522485ddf3cb46216058030a
Sagar Prabhu <your-actual-email@gmail.com>
2026-05-27 21:16:14 +0530

    Fix PostgreSQL settings

--- backend/breathe_esg/settings.py
+++ backend/breathe_esg/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -91,12 +92,22 @@ CORS_ALLOWED_ORIGINS = [
 
 AUTH_USER_MODEL = 'core.User'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.environ.get('DB_PATH', BASE_DIR / 'db.sqlite3'),
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=False,
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = ['*']
